b944d6d2fef47654b8c6407652d55b46ea42bca9
Giofadhil <giofadhil.ahmad@gmail.com>
2023-08-29 12:44:45 +0700

    feat: subscribe feature, CLI

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Device extends Model
 {
@@ -46,5 +47,50 @@ class Device extends Model
     public function subscribe_expression()
     {
         return $this->hasMany(SubscribeExpression::class, 'device_id');
+    }
+
+    public static function subscribeToTopic($mqtt, $topic)
+    {
+        $mqtt->subscribe($topic, function (string $topic, string $message) {
+            echo "Received message on topic: {$topic}\n";
+            echo "Received message with payload: {$message}\n";
+            try {
+                DB::transaction(function () use ($topic, $message) {
+
+                    // Create device log
+                    $device = Device::where('subscribe_topic', $topic)->first();
+                    $device_log = DeviceLog::create([
+                        'device_id' => $device->id,
+                        'value' => $message
+                    ]);
+
+                    // Subscribe Logic
+                    $subscribe_expression = $device->subscribe_expression;
+                    Device::evalValue($device->id, $device_log->id, $subscribe_expression, $message);
+
+                });
+                echo "Received message success!\n";
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+        }, 0);
+    }
+
+    public static function evalValue($device_id, $device_log_id, $subscribe_expression, $value)
+    {
+        foreach ($subscribe_expression as $val) {
+            $expression = str_replace("{{value}}", "'$value'", $val->expression);
+            if (eval("return $expression;")) {
+                DeviceStatus::updateOrCreate(
+                    [
+                        'device_id' => $device_id
+                    ],
+                    [
+                        'status_type_id' => $val->status_type_id,
+                        'device_log_id' => $device_log_id
+                    ]
+                );
+            }
+        }
     }
 }
