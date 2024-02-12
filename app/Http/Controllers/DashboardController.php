ae4abe2d957d0e5deb4700d79aa84009a7daf119
Giofadhil <giofadhil.ahmad@gmail.com>
2024-02-12 15:54:03 +0700

    fix: 12/2/2024 #5

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,49 +12,14 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        // filter by area get unique device_status
-        $status_type_widgets = StatusTypeWidget::with('status_type.device_status.device.publish_action')
-            ->with(['status_type.device_status.device' => function ($query) use ($request) {
-                if (!empty($request->locations)) {
-                    return $query->where(function ($w) use ($request) {
-                        $locations = $request->locations;
-                        foreach ($locations as $location) {
-                            $w->orWhere('branch', $location);
-                        }
-                    });
-                }
-                return $query;
-            }])
-            ->get()->sortByDesc('created_at');;
-
         $device_locations = Device::distinct()->get(['branch']);
         $device_sub_locations = Device::distinct()->get(['building']);
         $device_location_ids = Device::distinct()->get(['room']);
-
-        $status_types = [];
-
-        foreach ($status_type_widgets as $key => $status_type_widget) {
-            $status_types[$status_type_widget->status_type_id] = [
-                'widget_id' => $status_type_widget->id,
-                'name' => $status_type_widget->status_type->name,
-                'color' => $status_type_widget->status_type->color,
-                'count' => 0
-            ];
-
-            foreach ($status_type_widget->status_type->device_status as $key => $device_status) {
-                if ($device_status->device && $device_status->marked_as_read == false) {
-                    $status_types[$device_status->status_type_id]['count'] += 1;
-                }
-            }
-        }
-
-        $status_types = json_decode(json_encode($status_types));
 
         return view('admin.dashboard', compact(
             'device_locations',
             'device_sub_locations',
             'device_location_ids',
-            'status_types',
         ));
     }
 
@@ -94,9 +59,11 @@ class DashboardController extends Controller
                 }
                 return $query;
             }])
-            ->get()->sortByDesc('created_at');
+            ->orderBy('id', 'desc')
+            ->get();
 
         $absent_received_logs = [];
+
         if (Setting::first()->is_access_device) {
             $absent_received_logs = AbsentReceivedLog::with('absent_device', 'user')
                 ->whereHas('absent_device', function ($query) use ($request) {
